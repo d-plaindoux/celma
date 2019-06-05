a1b5781e673a2fea815477cc32190e8916e66bde
d-plaindoux <d.plaindoux@free.fr>
2019-06-05 11:14:37 +0200

    (wip) celma meta language parser done. AST transformation and compilation to Rust to be done

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -1,14 +1,19 @@
 extern crate proc_macro;
 
-mod parser;
-
 use proc_macro::TokenStream;
 
 use quote::quote;
 
+use celma::parser::parser::Parse;
+use celma::stream::char_stream::CharStream;
+
+mod parser;
+
 #[proc_macro]
-pub fn parsec<'a>(input: TokenStream) -> TokenStream {
-    println!("Parse [{}]", input.to_string());
+pub fn parsec(input: TokenStream) -> TokenStream {
+    let source = input.to_string();
+    let result = crate::parser::parsec().parse(CharStream::new(source.as_str()));
+    println!("Parse [{}]", result.fold(|_, _, _| true, |_| false));
 
     quote!(celma::parser::core::eos()).into()
 }
--- plugin/src/parser/mod.rs
+++ plugin/src/parser/mod.rs
@@ -1,26 +1,44 @@
 #![allow(dead_code)]
 
 use celma::parser::and::{AndOperation, AndProjection};
-use celma::parser::char::{char, char_in_set, digit, not_char};
+use celma::parser::char::{char, char_in_range, char_in_set, not_char};
+use celma::parser::core::parser;
 use celma::parser::fmap::FMapOperation;
+use celma::parser::lazy::lazy;
+use celma::parser::literal::string;
+use celma::parser::option::OptionalOperation;
+use celma::parser::or::OrOperation;
 use celma::parser::parser::{Combine, Parse};
 use celma::parser::repeat::RepeatOperation;
 use celma::stream::stream::Stream;
+
+use crate::parser::ASTParsec::{PBind, PChoice, PCode, PMap, POptional, PRepeat};
 
 // -------------------------------------------------------------------------------------------------
 // Grammar - Parser using Celma ^_^
 // -------------------------------------------------------------------------------------------------
 //
-// parser     ::= binding? atom occurrence? additional? transform?
+// parsec     ::= binding? atom occurrence? additional? transform?
 // binding    ::= IDENT '='
 // occurrence ::= ("*" | "+"  | "?")
-// additional ::= ("~" | "<~" | "~>" | "|") parser
+// additional ::= "|"? parsec
 // transform  ::= "=>" { -- rust code -- }
-// atom       ::= '(' parser ')' | CHAR | NUMBER | STRING | ^CHAR | { -- rust code -- }
+// atom       ::= '(' parsec ')' | { -- rust code -- }
 //
 // -------------------------------------------------------------------------------------------------
 // Note: Syn should be better but this done for dog-fooding purpose)
 // -------------------------------------------------------------------------------------------------
+
+#[derive(Clone, Debug)]
+pub enum ASTParsec {
+    PSequence(Box<ASTParsec>, Box<ASTParsec>),
+    PChoice(Box<ASTParsec>, Box<ASTParsec>),
+    POptional(Box<ASTParsec>),
+    PRepeat(bool, Box<ASTParsec>),
+    PBind(String, Box<ASTParsec>),
+    PCode(String),
+    PMap(Box<ASTParsec>, String),
+}
 
 #[inline]
 fn skip<'a, S: 'a>() -> impl Parse<(), S> + Combine<()> + Clone
@@ -32,41 +50,139 @@ where
         .fmap({ |_| () })
 }
 
+// -------------------------------------------------------------------------------------------------
+
 #[inline]
-fn delimiter_string<'a, S: 'a>() -> impl Parse<String, S> + Combine<String> + Clone
+pub fn parsec<'a, S: 'a>() -> impl Parse<ASTParsec, S> + Combine<ASTParsec> + Clone + 'a
 where
     S: Stream<Item = char>,
 {
-    char('"')
-        .and(not_char('"').opt_rep())
-        .right()
-        .and(char('"'))
+    binding()
+        .opt()
+        .and(skip())
         .left()
-        .fmap(|v| v.into_iter().collect::<String>())
+        .and(atom())
+        .and(skip())
+        .left()
+        .and(occurrence().opt())
+        .and(skip())
+        .left()
+        .and(additional().opt())
+        .and(skip())
+        .left()
+        .and(transform().opt())
+        .fmap(|((((bind, atom), occ), add), trans)| {
+            let bind = if bind.is_some() {
+                PBind(bind.unwrap(), Box::new(atom))
+            } else {
+                atom
+            };
+
+            let occ = if occ.is_some() {
+                match occ.unwrap() {
+                    '?' => POptional(Box::new(bind)),
+                    '*' => PRepeat(true, Box::new(bind)),
+                    '+' => PRepeat(false, Box::new(bind)),
+                    _ => bind,
+                }
+            } else {
+                bind
+            };
+
+            let add = if add.is_some() {
+                let value = add.unwrap();
+
+                if value.0 {
+                    PChoice(Box::new(occ), Box::new(value.1))
+                } else {
+                    occ
+                }
+            } else {
+                occ
+            };
+
+            let trans = if trans.is_some() {
+                PMap(Box::new(add), trans.unwrap())
+            } else {
+                add
+            };
+
+            trans
+        })
 }
 
 #[inline]
-fn number<'a, S: 'a>() -> impl Parse<i32, S> + Combine<i32> + Clone
+fn binding<'a, S: 'a>() -> impl Parse<String, S> + Combine<String> + Clone + 'a
 where
     S: Stream<Item = char>,
 {
-    digit()
+    let letter = char_in_range('A'..'Z').or(char_in_range('a'..'z'));
+
+    letter
         .rep()
-        .fmap(|v| v.into_iter().collect::<String>())
-        .fmap(|s| s.parse::<i32>().unwrap())
+        .and(skip())
+        .left()
+        .and(char('='))
+        .left()
+        .fmap(|v| v.into_iter().collect())
 }
 
 #[inline]
-fn delimited_char<'a, S: 'a>() -> impl Parse<char, S> + Combine<char> + Clone
+fn occurrence<'a, S: 'a>() -> impl Parse<char, S> + Combine<char> + Clone + 'a
 where
     S: Stream<Item = char>,
 {
-    char('\'')
-        .and(not_char('\''))
-        .right()
-        .and(char('\''))
+    char_in_set(vec!['+', '?', '*'])
+}
+
+fn additional<'a, S: 'a>(
+) -> impl Parse<(bool, ASTParsec), S> + Combine<(bool, ASTParsec)> + Clone + 'a
+where
+    S: Stream<Item = char>,
+{
+    char('|')
+        .opt()
+        .fmap(|o| o.is_some())
+        .and(skip())
         .left()
-        .fmap(|c| c)
+        .and(lazy(|| parser(parsec())))
+}
+
+#[inline]
+fn atom<'a, S: 'a>() -> impl Parse<ASTParsec, S> + Combine<ASTParsec> + Clone + 'a
+where
+    S: Stream<Item = char>,
+{
+    char('(')
+        .and(skip())
+        .left()
+        .and(lazy(|| parser::<'a, _, ASTParsec, S>(parsec())))
+        .right()
+        .and(skip())
+        .left()
+        .and(char(')'))
+        .left()
+        .or(code().fmap(PCode))
+}
+
+#[inline]
+fn transform<'a, S: 'a>() -> impl Parse<String, S> + Combine<String> + Clone + 'a
+where
+    S: Stream<Item = char>,
+{
+    string("=>").and(skip()).left().and(lazy(code)).right()
+}
+
+fn code<'a, S: 'a>() -> impl Parse<String, S> + Combine<String> + Clone + 'a
+where
+    S: Stream<Item = char>,
+{
+    char('{')
+        .and(not_char('}').opt_rep())
+        .right()
+        .and(char('}'))
+        .left()
+        .fmap(|v| v.into_iter().collect())
 }
 
 // -------------------------------------------------------------------------------------------------
